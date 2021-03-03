20e8bbc23929a076f947108ce95dbd9684318344
Elvis <elvis.e.amancio@gmail.com>
2021-03-03 17:33:55 -0300

    Ajustes

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,11 @@
 
-export function signIn(email: string, password:string,){
+export function signIn(email: string, password: string,) {
 
     return ({
-                token: password+email, // that will be change ofc
-                user: {
-                    email: email,
-                    password: password,
-                },
-            });
+        token: password + email, // that will be change ofc
+        user: {
+            email: email,
+            password: password,
+        },
+    });
 }
